2ab8c481da849eb03f6c7b826f20c77fda98d680
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-05-10 15:09:31 +0530

    new commits

--- server.py
+++ server.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# @app.route('/button', methods=['POST'])
+# def download():
+#     # this method takes the content of the text area and the filename and then saves it to a file
+#     content = request.form['content']
+#     filename = request.form['filename']
+#     with open(filename, 'w') as f:
+#         f.write(content)
+#     return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
